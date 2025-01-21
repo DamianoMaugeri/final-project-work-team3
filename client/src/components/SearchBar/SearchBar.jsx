4611487318c5b5c1d3a1839de89d376a1d50691d
Arela-Cristina <instagrandine@gmail.com>
2025-01-21 17:17:37 +0100

    add style

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 export default function SearchBar() {
     return (
-        <div className="container mt-5">
+        <div className="container mt-5 mb-5">
             <div className="input-group">
                 <input
                     type="text"
