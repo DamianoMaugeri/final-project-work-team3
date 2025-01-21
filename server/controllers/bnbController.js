9bb44d22838fd7a0a07b92322ce187b35a355488
DamianoMaugeri <damianomaugeri97@icloud.com>
2025-01-21 10:09:55 +0100

    creata connection nel controller

--- server/controllers/bnbController.js
+++ server/controllers/bnbController.js
@@ -1,7 +1,19 @@
-
+const connection = require('../data/db.js');
 
 
 function index(req, res) {
+    let sql = ` SELECT * 
+    FROM  properties`
+
+    connection.query(sql, (err, properties) => {
+        // console.log(err)
+        if (err) return res.status(500).json({ message: err.message })
+
+
+
+
+        res.json(properties)
+    })
     console.log(res)
 }
 
