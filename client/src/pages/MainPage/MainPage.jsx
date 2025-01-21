f9b2c0058a2e01d320ea17c6130379cebe8b0ee4
Arela-Cristina <instagrandine@gmail.com>
2025-01-21 16:01:10 +0100

    add style

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,13 @@
 import HouseList from "../../components/HouseList/HouseList";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import style from "./MainPage.module.css";
 
 export default function MainPage() {
 
     return (
-        <div>
-            <SearchBar />
-            <HouseList />
-
+        <div className="d-flex">
+            <SearchBar  className={style.searchBar}/>
+            <HouseList className={style.houseList}/>
         </div>
     )
 }
