724ea08bf93c4239680a6dcfb45ef16dcd81df87
Arela-Cristina <instagrandine@gmail.com>
2025-01-21 13:17:31 +0100

    feat: import use state from react, create and render House card. Add toggle functionality

--- client/src/components/HouseCard/HouseCard.jsx
+++ client/src/components/HouseCard/HouseCard.jsx
@@ -1,9 +1,55 @@
+import React, { useState } from "react";
+import { HeartIcon as OutlineHeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as SolidHeartIcon } from "@heroicons/react/24/solid";
+
+
+
 export default function HouseCard({ content }) {
 
-    const { title } = content
+    const { title, full_adress, city, image } = content
 
+    // stato per i cambiamenti del  cuoricino
+    const [liked, setLiked] = useState(false);
+
+    // funzione toogle per il cuoricino
+    const handleLikeToggle = () => {
+        setLiked(!liked); //se liked è true diventa false e viceversa
+    };
 
     return (
-        <div>{title}</div>
+
+        <div className="card" style={{ width: "18rem", margin: "1rem" }}>
+            {/* immagine */}
+            <div className="position-relative">
+                <img
+                    src={image}
+                    className="card-img-top"
+                    alt="Placeholder"
+                />
+                {/* Cuoricino */}
+                <button
+                    onClick={handleLikeToggle}
+                    className="btn btn-light position-absolute top-0 end-0 m-2 p-2 rounded-circle"
+                    style={{ boxShadow: "0 2px 5px rgba(0,0,0,0.2)" }}
+                >
+
+                    {liked ? (
+                        <SolidHeartIcon className="text-danger" style={{ height: "1.5rem" }} />
+                    ) : (
+                        <OutlineHeartIcon className="text-dark" style={{ height: "1.5rem" }} />
+                    )}
+                </button>
+            </div>
+
+            {/* contenuto della card */}
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">
+                    {full_adress}
+                </p>
+                <p className="card-text">{city}</p>
+            </div>
+        </div>
+
     )
 }
