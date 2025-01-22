451b1c925e61724a1641e17c7de3294e69a8793b
Arela-Cristina <instagrandine@gmail.com>
2025-01-22 10:46:33 +0100

    feat: fetch whit Axios

--- client/src/pages/ShowPage/ShowPage.jsx
+++ client/src/pages/ShowPage/ShowPage.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react"
+import axios from "axios"
 import ReviewCard from "../../components/Review Card/ReviewCard"
 
 
 export default function ShowPage() {
+
+    // recuperare l'id del libro dal path della rotta
+    const [house, setHouse] = useState(null)
+
+    const { id } = useParams()
+
+
+    function fetchHouse() {
+        axios.get(`http://localhost:3000/api/boolbnb/${id}`)
+            .then(res => {
+                setHouse(res.data)
+                console.log('data', data)
+            })
+            .catch(err => {
+                console.error(err)
+                // qui dovremmo fare un redirect alla pagina 404
+            })
+            
+    }
+
+    useEffect(() => {
+        fetchMovie()
+    }, [id])
+
+
     return (
         <>
 
             {/* detagli casa */}
-
-            <div>show page</div>
-            <div>
+            <div className="d-flex">
                 <div>
                     <img src="" alt="" />
                 </div>
