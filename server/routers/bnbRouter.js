e5b8d2d057ebf3d5ef55da776d30f41224659516
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-22 16:56:00 +0100

    add=> commenti nel bnbRouter

--- server/routers/bnbRouter.js
+++ server/routers/bnbRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
-
+//separazione dei controller per dividere le logiche tra: 
+//User (può visualizzare tutte le proprietà, aggiungere voti ad una proprietà,aggiungere review ad una proprietà solo se ha già preso in affitto quella specifica proprietà) 
+//Owner (può visualizzare solo le proprietà già aggiunte sulla piattaforma, può aggiungere altre proprietà)
 const bnbUserController = require('../controllers/bnbUserController.js')
 const bnbOwnerController = require('../controllers/bnbOwnerController.js')
 
-// rotte USER
+// rotte => USER
 
 // index: mostra tutte le properties
 router.get('/', bnbUserController.index);
@@ -13,15 +15,15 @@ router.get('/', bnbUserController.index);
 // show: mostra la singola property
 router.get('/:id([0-9]+)', bnbUserController.show);
 
-//update: aggiorna parzialmente properties
+//update: aggiorna parzialmente properties (utilizzata per aggiornare il campo vote)
 router.patch('/:id([0-9]+)', bnbUserController.update);
 
 //postReview: permette di inserire una review solo se l'utente ha una prenotazione e se non ha già lasciato una review (controllo fatto tramite email dell'user)
 router.post('/:id([0-9]+)/reviews', bnbUserController.postReview);
 
-// rotte OWNER
+// rotte => OWNER
 
-// propertiesByOwner: mostra tutte le proprietà di un owner specifico
+// propertiesByOwner: questa rotta simula un autenticazione tramite sola email e nel caso in cui ci sia corrispondenza con quella di un proprietario restituisce tutte le proprietà appartenenti a quel proprietario
 router.get('/owner/:id([0-9]+)', bnbOwnerController.propertiesByOwner);
 
 // permette agli owner di creare una nuova proprietà
