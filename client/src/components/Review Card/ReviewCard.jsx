36917e3a796e0b73cdc2452427c273e383c852d2
Arela-Cristina <instagrandine@gmail.com>
2025-01-22 10:21:19 +0100

    create ReviewCard componentt and give a structure for ShowPage

---
+++ client/src/components/Review Card/ReviewCard.jsx
@@ -0,0 +1,11 @@
+export default function ReviewCard() {
+    return (
+        <div>
+            <p>recenzione</p>
+            <div>
+                <strong>Voto</strong>
+            </div>
+            <div>Name</div>
+        </div>
+    )
+}
