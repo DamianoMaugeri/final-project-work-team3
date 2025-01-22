ff42669c7d5e594e8f6b1b7f497866299d56a982
Arela-Cristina <instagrandine@gmail.com>
2025-01-22 12:12:38 +0100

    feat: set props on ReviewCard

--- client/src/components/Review Card/ReviewCard.jsx
+++ client/src/components/Review Card/ReviewCard.jsx
@@ -1,11 +1,24 @@
-export default function ReviewCard() {
+export default function ReviewCard({ reviews }) {
+
+
+
     return (
-        <div>
-            <p>recenzione</p>
-            <div>
-                <strong>Voto</strong>
-            </div>
-            <div>Name</div>
-        </div>
-    )
+        reviews ?
+            <div className="mt-5">
+                {reviews.map((review, index) => (
+                    <div key={index} className="review-card">
+                        <p>{review.text}</p>
+                        <div>
+                            <strong>Voto: {review.vote}</strong>
+                            <div>
+                                Check In: {new Date(review.rent_start).toLocaleDateString()} - Check Out: {new Date(review.rent_end).toLocaleDateString()}
+                            </div>
+                        </div>
+                        <div>{review.first_name} {review.last_name}</div>
+                    </div>
+                ))}
+            </div> :
+            <div>nessun risultato </div>
+    );
+
 }
