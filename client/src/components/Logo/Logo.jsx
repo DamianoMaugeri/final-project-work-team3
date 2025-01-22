df10b272ff82064a0e02627716ba0f6192eb9e3f
Arela-Cristina <instagrandine@gmail.com>
2025-01-22 12:52:12 +0100

    feat: set Link from react router dom to redirect to Home Page

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import LogoBoolBnb from "../../assets/logo.jpg";
+import { Link } from 'react-router-dom';
 import style from "./Logo.module.css";
 
 export default function Logo() {
     return (
         <div className="d-flex justify-content-center mt-5 mb-4">
-            <img  src={LogoBoolBnb} alt="Logo Boolbnb" />
+            <Link to="/">
+                <img src={LogoBoolBnb} alt="Logo Boolbnb" />
+            </Link>
         </div>
     )
 }
