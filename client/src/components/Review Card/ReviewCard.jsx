6106f5bd84e5855834f76d3463f14774adf60623
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-22 12:29:52 +0100

    add style on review card component

--- client/src/components/Review Card/ReviewCard.jsx
+++ client/src/components/Review Card/ReviewCard.jsx
@@ -4,20 +4,31 @@ export default function ReviewCard({ reviews }) {
 
     return (
         reviews ?
-            <div className="mt-5">
-                {reviews.map((review, index) => (
-                    <div key={index} className="review-card">
-                        <p>{review.text}</p>
-                        <div>
-                            <strong>Voto: {review.vote}</strong>
-                            <div>
-                                Check In: {new Date(review.rent_start).toLocaleDateString()} - Check Out: {new Date(review.rent_end).toLocaleDateString()}
-                            </div>
+            <section className="container mt-4">
+                <div>
+                    <h3 className="card-title mb-2">Recensioni degli affittuari:</h3>
+                </div>
+                <div className="card" >
+                    <div className="card-body" >
+                        <div className="row">
+                            {reviews.map((review, index) => (
+                                <div key={index} className="col">
+                                    <p className="card-text">{review.text}</p>
+                                    <div>
+                                        <p className="card-text"><strong>Valutazione:</strong> {review.vote}</p>
+                                        <div>
+                                            <p className="card-text"><strong>Check In:</strong> {new Date(review.rent_start).toLocaleDateString()}</p>
+                                            <p className="card-text"><strong>Check Out: </strong> {new Date(review.rent_end).toLocaleDateString()}</p>
+                                        </div>
+                                    </div>
+                                    <p className="card-text" ><strong>Scritta da: </strong>{review.first_name} {review.last_name}</p>
+                                </div>
+                            ))}
                         </div>
-                        <div>{review.first_name} {review.last_name}</div>
                     </div>
-                ))}
-            </div> :
+                </div>
+            </section>
+            :
             <div>nessun risultato </div>
     );
 
