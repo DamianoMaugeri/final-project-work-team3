b1405e3f7655841fcccf527c18361de169ab4c9b
Arela-Cristina <instagrandine@gmail.com>
2025-01-22 12:48:35 +0100

    add style for Logo

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,5 +1,10 @@
+import LogoBoolBnb from "../../assets/logo.jpg";
+import style from "./Logo.module.css";
+
 export default function Logo() {
     return (
-        <div>Logo</div>
+        <div className="d-flex justify-content-center mt-5 mb-4">
+            <img  src={LogoBoolBnb} alt="Logo Boolbnb" />
+        </div>
     )
 }
