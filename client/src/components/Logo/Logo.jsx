5cb0f1ebf209ba537d77aeca6316631f6cc6680e
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-23 12:23:41 +0100

    add logo logic

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,11 +1,17 @@
 import LogoBoolBnb from "../../assets/logo.png";
 import { Link } from 'react-router-dom';
 import style from "./Logo.module.css";
+import { useContext } from "react";
+import GlobalContext from "../../context/GlobalContext";
 
 export default function Logo() {
+    const { setSearchedCity } = useContext(GlobalContext)
     return (
         <div className="d-flex justify-content-center mt-5 mb-4">
-            <Link to="/">
+            <Link to="/" onClick={() => {
+                setSearchedCity("")
+                fetchHouses()
+            }}>
                 <img style={{ width: "200px" }} src={LogoBoolBnb} alt="Logo Boolbnb" />
             </Link>
         </div>
