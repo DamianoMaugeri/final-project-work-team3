11cb0968c8989045f5feda713bb1ce911a232f93
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-23 16:04:06 +0100

    add style on owner show page

--- client/src/pages/OwnerShowPage/OwnerShowPage.jsx
+++ client/src/pages/OwnerShowPage/OwnerShowPage.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import GlobalContext from "../../context/GlobalContext"
 import HouseCard from "../../components/HouseCard/HouseCard"
 import HouseForm from "../../components/HouseForm/HouseForm"
+import style from './OwnerShowpage.module.css'
 
 export default function OwnerShowpage() {
 
@@ -17,21 +18,46 @@ export default function OwnerShowpage() {
     return (
         owner &&
         <>
-            <div>
-                <div>
-                    {first_name}
-                </div>
-                <div> {last_name} </div>
-                <div> {email} </div>
+            <section className="container mt-4">
+                <div className={`card ${style.customCard}`}>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col">
+                                <h1 className="card-title">
+                                    {first_name} {last_name}
+                                </h1>
+                                <h6 className="card-subtitle mb-4 text-white">
+                                    {email}
+                                </h6>
+                            </div>
+                        </div>
 
-                <div>
-                    {propertiesOwned.map((property, i) => (
-                        <div key={i}> <HouseCard content={property} /> </div>
-                    ))}
-                </div>
-            </div>
-            <HouseForm />
+                        <div className="row">
+                            <div className="col">
+                                <h3 className="mb-3">I tuoi immobili:</h3>
+                                {propertiesOwned.length > 0 ? (
+                                    <div className="d-flex flex-wrap gap-3">
+                                        {propertiesOwned.map((property, i) => (
+                                            <div key={i} className="col-12 col-md-6 col-lg-4">
+                                                <HouseCard content={property} />
+                                            </div>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <p className="text-muted">Nessuna proprietà trovata.</p>
+                                )}
+                            </div>
+                        </div>
 
+                        <div className="row mt-4">
+                            <h1>Aggiungi un nuovo immobile:</h1>
+                            <div className="col mt-4">
+                                <HouseForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
 
     )
