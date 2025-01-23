040b086d41b9bc616832fdb208fbcd0858cd54ff
Arela-Cristina <instagrandine@gmail.com>
2025-01-23 13:27:41 +0100

    modify ReviewForm HTML  structure and add style

--- client/src/components/ReviewForm/ReviewForm.jsx
+++ client/src/components/ReviewForm/ReviewForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import axios from "axios"
+import style from './ReviewForm.module.css'
 
 const initialFormData = {
     vote: '',
@@ -67,43 +68,45 @@ export default function ReviewForm({ id, onSuccess = () => { } }) {
 
 
     return (
-        <form  className="container mt-4" onSubmit={storeReview}>
+        <section>
+            <form className={`container mt-4 ${style.customForm}`} onSubmit={storeReview}>
 
-            <p>
-                <label htmlFor="email" className="form-label">EMAIL *</label>
-                <input required type="email" className="form-control" placeholder="inserisci l'email" name="email" id="email" value={formData.email} onChange={handleForm} />
+                <p>
+                    <label htmlFor="email" className="form-label">EMAIL *</label>
+                    <input required type="email" className="form-control" placeholder="inserisci l'email" name="email" id="email" value={formData.email} onChange={handleForm} />
 
-            </p>
+                </p>
 
-            <p className='form-control'>
-                <label htmlFor="text" className="form-label" >Recensione</label>
-                <textarea rows="4" name="text" id="text" placeholder='Scrivi la tua recensione' className="form-control" value={formData.text} onChange={handleForm}></textarea>
-            </p>
+                <p className='form-control'>
+                    <label htmlFor="text" className="form-label" >Recensione</label>
+                    <textarea rows="4" name="text" id="text" placeholder='Scrivi la tua recensione' className="form-control" value={formData.text} onChange={handleForm}></textarea>
+                </p>
 
-            <p className='form-control'>
-                <label htmlFor="vote" className="form-label" >Voto *</label>
-                <select required name="vote" id="vote" className="form-control" value={formData.vote} onChange={handleForm}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                </select>
-            </p>
+                <p className='form-control'>
+                    <label htmlFor="vote" className="form-label" >Voto *</label>
+                    <select required name="vote" id="vote" className="form-control" value={formData.vote} onChange={handleForm}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
+                </p>
 
 
 
-            <div>
-                {isFormValid === false && <div>i dati non sono validi</div>}
-                <button className="submit-button">invia</button>
-            </div>
+                <div>
+                    {isFormValid === false && <div>i dati non sono validi</div>}
+                    <button className="submit-button">invia</button>
+                </div>
 
-        </form>
+            </form>
+        </section>
     )
 
 }
