60e9b2b7a6f2d9dd8c123d966f3dd29f822f8514
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-26 13:09:43 +0100

    add da modificare

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -3,12 +3,28 @@ import style from './Filters.module.css'
 
 export default function Filters() {
     return (
-        <ul className={`${style.filters} p-0`} >
-            <li>filtro1</li>
-            <li>filtro2</li>
-            <li>filtro3</li>
-            <li>filtro4</li>
-            <li>filtro5</li>
-        </ul>
+        <div className={`${style.filters} p-0`} >
+            <button>Filtri avanzati</button>
+            <div className='d-flex justify-content-start'>
+                <input type="radio" id='roomsNumber' name='filters' />
+                <label htmlFor="roomsNumber">Numero di stanze</label>
+
+            </div>
+            <div className='d-flex flex-start'>
+                <input type="radio" id='roomsNumber' name='filters' />
+                <label htmlFor="roomsNumber">Posti letto</label>
+
+            </div>
+            <div className='d-flex'>
+                <input type="radio" id='roomsNumber' name='filters' />
+                <label htmlFor="roomsNumber">Bagni</label>
+
+            </div>
+            <div className='d-flex'>
+                <input type="radio" id='roomsNumber' name='filters' />
+                <label htmlFor="roomsNumber">Dimensioni</label>
+
+            </div>
+        </div>
     )
 }
