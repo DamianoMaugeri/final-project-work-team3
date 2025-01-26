7bf57a6e5edb873dd235000ee303cacfad1fb41b
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-01-26 13:20:05 +0100

    add

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,6 +1,6 @@
 import style from './Filters.module.css'
 
-
+// i filtri sono questi ma non va bene l'idea di utilizzare i radio button meglio cambiare con <FontAwesomeIcon icon="fa-solid fa-circle-chevron-down" rotation={270} /> quando il filtro è chiuso e <FontAwesomeIcon icon="fa-solid fa-circle-chevron-down" />
 export default function Filters() {
     return (
         <div className={`${style.filters} p-0`} >
@@ -23,6 +23,11 @@ export default function Filters() {
             <div className='d-flex'>
                 <input type="radio" id='roomsNumber' name='filters' />
                 <label htmlFor="roomsNumber">Dimensioni</label>
+
+            </div>
+            <div className='d-flex'>
+                <input type="radio" id='roomsNumber' name='filters' />
+                <label htmlFor="roomsNumber">Prezzo giornaliero</label>
 
             </div>
         </div>
