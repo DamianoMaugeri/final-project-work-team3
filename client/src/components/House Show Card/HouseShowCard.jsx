ecdd5f36c9abf2528f9d41ecba10c98546c6b52f
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-28 10:40:27 +0100

    add vote on  house show card

--- client/src/components/House Show Card/HouseShowCard.jsx
+++ client/src/components/House Show Card/HouseShowCard.jsx
@@ -3,7 +3,7 @@ import placeHolder from '../../assets/placeholder.png'
 export default function HouseShowCard({ houseEl }) {
 
 
-    const { title, number_of_rooms, number_of_beds, number_of_bathrooms, size, city, full_address, image } = houseEl
+    const { title, vote, number_of_rooms, number_of_beds, number_of_bathrooms, size, city, full_address, image } = houseEl
 
     return (
         houseEl ?
@@ -25,13 +25,13 @@ export default function HouseShowCard({ houseEl }) {
                                         <h2 className="card-title mb-4">{title}</h2>
                                         <h5 className="card-text">Dettagli dell'immobile:</h5>
                                         <ul>
-                                            <li className="card-text"><strong>Valutazione:</strong> </li>
-                                            <li className="card-text" ><strong>Indirizzo:</strong> {full_address}</li>
-                                            <li className="card-text"><strong>Stanze:</strong> {number_of_rooms}</li>
-                                            <li className="card-text"><strong>Posti letto:</strong> {number_of_beds}</li>
-                                            <li className="card-text"><strong>Bagni:</strong> {number_of_bathrooms}</li>
-                                            <li className="card-text"><strong>Dimensione:</strong> {size} mq</li>
-                                            <li className="card-text"><strong>Città:</strong> {city}</li>
+                                            <li className="card-text">Valutazione: {vote} </li>
+                                            <li className="card-text" >Indirizzo: {full_address}</li>
+                                            <li className="card-text">Stanze: {number_of_rooms}</li>
+                                            <li className="card-text">Posti letto: {number_of_beds}</li>
+                                            <li className="card-text">Bagni: {number_of_bathrooms}</li>
+                                            <li className="card-text">Dimensione: {size} mq</li>
+                                            <li className="card-text">Città: {city}</li>
                                         </ul>
                                     </div>
                                 </div>
