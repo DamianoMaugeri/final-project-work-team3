209b9fd532a894f1719fdfaa83e0f1062d17c7eb
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-02-03 17:20:58 +0100

    add button on show page for rent the house

--- client/src/pages/ShowPage/ShowPage.jsx
+++ client/src/pages/ShowPage/ShowPage.jsx
@@ -1,87 +1,81 @@
-import HouseShowCard from "../../components/House Show Card/HouseShowCard"
-import ReviewCard from "../../components/Review Card/ReviewCard"
-import ReviewForm from "../../components/ReviewForm/ReviewForm"
-import { useState, useEffect } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import axios from "axios"
-import style from "./ShowPage.module.css"
-import Loader from "../../components/Loader/Loader"
-import EmailForm from "../../components/emailForm/emailForm"
-import HeaderMain from "../../components/HeaderMain/HeaderMain"
-import RentForm from "../../components/RentForm/RentForm"
+import HouseShowCard from "../../components/House Show Card/HouseShowCard";
+import ReviewCard from "../../components/Review Card/ReviewCard";
+import ReviewForm from "../../components/ReviewForm/ReviewForm";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import style from "./ShowPage.module.css";
+import Loader from "../../components/Loader/Loader";
+import EmailForm from "../../components/emailForm/emailForm";
+import HeaderMain from "../../components/HeaderMain/HeaderMain";
+import RentForm from "../../components/RentForm/RentForm";
 
 export default function ShowPage() {
-
-
-    const [house, setHouse] = useState(null)
-    const [reviewBoolean, setReviewBoolean] = useState("")
-    const [emailBoolean, setEmailBoolean] = useState("")
-    const { slug } = useParams()
-    // const nuovoid = location.state.id
-
-
-    function fetchHouse() {
-
-        console.log(slug)
-        axios.get(`http://localhost:3000/api/boolbnb/${slug}`)
-            .then(res => {
-                setHouse(res.data)
-                console.log('data', res.data)
-
-
-            })
-            .catch(err => {
-                console.error(err)
-                // qui dovremmo fare un redirect alla pagina 404
-            })
-
-    }
+    const [house, setHouse] = useState(null);
+    const [reviewBoolean, setReviewBoolean] = useState(false);
+    const [emailBoolean, setEmailBoolean] = useState(false);
+    const [rentBoolean, setRentBoolean] = useState(false);
+    const { slug } = useParams();
 
     useEffect(() => {
-        fetchHouse()
-    }, [slug])
-
+        axios.get(`http://localhost:3000/api/boolbnb/${slug}`)
+            .then(res => setHouse(res.data))
+            .catch(err => console.error(err));
+    }, [slug]);
 
     return (
-        // ShowPage div container
         <div className={`d-flex flex-column flex-grow-1 mt-5 pt-5 ${style.showPageContainer}`}>
             <HeaderMain />
-            {house ? ( //if lenght > 0 render ShowPage else render loader inside  the ShowPage div container
+            {house ? (
                 <>
                     <HouseShowCard houseEl={house} />
-                    {/* recensioni box */}
 
 
-                    <div className="d-flex justify-content-around mb-5 mt-5">
-                        <button className={`${style.btn} ${reviewBoolean ? style['btn-active'] : ''}`} onClick={() => {
-                            emailBoolean && setEmailBoolean(!emailBoolean)
-                            setReviewBoolean(!reviewBoolean)
-                        }}>Lascia una recensione</button>
+                    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-3 mb-5 mt-5">
+                        <button
+                            className={`${style.btn} ${emailBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setReviewBoolean(false);
+                                setRentBoolean(false);
+                                setEmailBoolean(!emailBoolean);
+                            }}>
+                            Contatta il proprietario
+                        </button>
 
-                        <button className={`${style.btn} ${emailBoolean ? style['btn-active'] : ''}`} onClick={() => {
-                            reviewBoolean && setReviewBoolean(!reviewBoolean)
-                            setEmailBoolean(!emailBoolean)
-                        }}>Contatta il proprietario</button>
+                        <button
+                            className={`${style.btn} ${rentBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setReviewBoolean(false);
+                                setEmailBoolean(false);
+                                setRentBoolean(!rentBoolean);
+                            }}>
+                            Prenota adesso
+                        </button>
+
+                        <button
+                            className={`${style.btn} ${reviewBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setEmailBoolean(false);
+                                setRentBoolean(false);
+                                setReviewBoolean(!reviewBoolean);
+                            }}>
+                            Lascia una recensione
+                        </button>
                     </div>
-                    {/* form recensioni */}
-                    {!emailBoolean && reviewBoolean && <ReviewForm id={house.id} />}
 
-                    {!reviewBoolean && emailBoolean && <EmailForm email={house.ownerEmail} />}
 
-                    <RentForm id={house.id} />
+                    {reviewBoolean && <ReviewForm id={house.id} />}
+                    {emailBoolean && <EmailForm email={house.ownerEmail} />}
+                    {rentBoolean && <RentForm id={house.id} />}
 
                     <hr />
                     <ReviewCard reviews={house.reviews} />
                 </>
-            ) :
-
-                // ... else render loader inside  the ShowPage div container
-                (
-                    <div className="d-flex align-items-center justify-content-center flex-grow-1">
-                        <Loader />
-                    </div>
-                )}
+            ) : (
+                <div className="d-flex align-items-center justify-content-center flex-grow-1">
+                    <Loader />
+                </div>
+            )}
         </div>
-
-    )
-} 
+    );
+}
